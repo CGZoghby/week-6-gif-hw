19659c847997a22e7dad739ec225830e32ddf509
Christopher Zoghby <cgzoghby@gmail.com>
2018-03-08 05:33:38 -0500

    restylized layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,58 @@
 $(document).ready(function () {
-
-    // Initial array of animals
-    var animals = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
-
     // displayanimalInfo function re-renders the HTML to display the appropriate content
+    //"http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5" <-- example
+    var animals = [];
+
     function displayanimalInfo() {
         var animal = $(this).attr("data-name");
-        var queryURL = "https://www.omdbapi.com/?t=" + animal + "&y=&plot=short&apikey=trilogy";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=p8zcPAjaO8q33EgfKVKEreHhlnOUlHep&limit=16";
         // Creates AJAX call for the specific animal button being clicked
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // YOUR CODE GOES HERE!!!
-            $("#animals-view").prepend(
-                "<br><img src=" + response.Poster + ">" +
-                "<br>Title: " + response.Title +
-                "<br>Actors: " + response.Actors +
-                "<br>Rating: " + response.Rated +
-                "<br>Plot: " + response.Plot +
-                "<br>Release Date: " + response.Released +
-                "<br><br>");
+            // First empty animals viewport
+            $("#animals-view").empty();
+            //then populate with new stuff
+            for (var i = 0; i < response.data.length; i++) {
+                columnDiv = $("<div>") //adding column divs with bootstrap neatly controls the layout of the gifs
+                animalBigDiv = $("<div>"); //this adds an additional level of aesthetic by displaying all gifs on bootstrap's new "cards"
+                animalImg = $("<img>"); //adds img src
+                animalSmallDiv = $("<div>") //will contain the rating of the attached gif
+                columnDiv.addClass("col-md-3");
+                animalBigDiv.addClass("card");
+                animalImg.addClass("card-img-top");
+                animalImg.attr("src", response.data[i].images.downsized.url);
+                animalImg.attr("alt", animal + ".gif");
+                animalSmallDiv.addClass("card-body")
+                animalSmallDiv.append(`<h5 class="card-title"> Rating: ${response.data[i].rating} </h5>`);
+                animalImg.append(animalSmallDiv);
+                animalBigDiv.append(animalImg);
+                columnDiv.append(animalBigDiv);
+                $("#animals-view").append(columnDiv);
+            };
         });
-    }
-    // Function for displaying animal data
+    };
+
     function renderButtons() {
-        // Deletes the animals prior to adding new animals
+        // Deleting the animals prior to adding new animals
         // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
-        // Loops through the array of animals
+        // Looping through the array of animals
         for (var i = 0; i < animals.length; i++) {
-            // Then dynamicaly generates buttons for each animal in the array
+            // Then dynamicaly generating buttons for each animal in the array
             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var a = $("<button>");
-            // Adds a class of animal to our button
-            a.addClass("animal");
-            // Added a data-attribute
+            // Adding a class of animal-btn to our button
+            a.addClass("animal-btn");
+            // Adding a data-attribute
             a.attr("data-name", animals[i]);
-            // Provided the initial button text
+            // Providing the initial button text
             a.text(animals[i]);
-            // Added the button to the buttons-view div
+            // Adding the button to the buttons-view div
             $("#buttons-view").append(a);
-        };
-    };
+        }
+    }
     // This function handles events where the add animal button is clicked
     $("#add-animal").on("click", function (event) {
         event.preventDefault();
@@ -53,8 +63,7 @@ $(document).ready(function () {
         // Calling renderButtons which handles the processing of our animal array
         renderButtons();
     });
+
     // Adding click event listeners to all elements with a class of "animal"
-    $(document).on("click", ".animal", displayanimalInfo);
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+    $(document).on("click", ".animal-btn", displayanimalInfo);
 });
